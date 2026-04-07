df4df591ae4b43785525426eb91bfcb4292a4081
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-04-07 13:55:27 +0100

    Allowing for distinct zones to exist.

--- src/main/java/net/gourmand/core/registry/worldgen/OthersideBiomeSource.java
+++ src/main/java/net/gourmand/core/registry/worldgen/OthersideBiomeSource.java
@@ -2,6 +2,7 @@ package net.gourmand.core.registry.worldgen;
 
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.gourmand.core.AncientGroundCore;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.QuartPos;
@@ -41,18 +42,29 @@ public class OthersideBiomeSource extends BiomeSource {
     }
 
     public Holder<Biome> getNoiseBiome(int x, int y, int z, Climate.Sampler sampler) {
-        int i = QuartPos.toBlock(x);
-        int j = QuartPos.toBlock(y);
-        int k = QuartPos.toBlock(z);
-        int l = SectionPos.blockToSectionCoord(i);
-        int i1 = SectionPos.blockToSectionCoord(k);
+        int i = QuartPos.toBlock(x); // Multiplies x by 4
+        int j = QuartPos.toBlock(y); // Multiplies x by 4
+        int k = QuartPos.toBlock(z); // Multiplies x by 4
+        int l = SectionPos.blockToSectionCoord(i);  // (x * 4)/16
+        int i1 = SectionPos.blockToSectionCoord(k);  // (z * 4)/16
 
-        long d = (long)l * l + (long) i1 * i1;
-        long d1 = 4096L;
+        long d = ((long)l * l + (long) i1 * i1); // ((x * 4)/16)^2 + ((z * 4)/16)^2. creates a circular shape.
+        long d1 = d >> 4;
 
-        if (d <= d1) return this.end;
-        if (d <= d1 * 2) return this.highlands;
-        if (d <= d1 * 3) return this.midlands;
+        long THRESHOLD = 4096L;
+
+        // keeping debug in, but set false in commited versions.
+        if (y == 50 && false){
+            AncientGroundCore.LOGGER.info("{} {} {} {} C: {} {} {}", d, l * l, i1 * i1, l, x, y, z);
+        }
+
+        if (d <= THRESHOLD) return this.end;
+        if (d1 >> 1 <= THRESHOLD) return this.islands;
+        // from here on each zone is twice the size of the previous.
+        if (d1 >> 2 <= THRESHOLD) return this.highlands;
+        if (d1 >> 3 <= THRESHOLD) return this.midlands;
+        if (d1 >> 4 <= THRESHOLD) return this.highlands;
+        if (d1 >> 5 <= THRESHOLD) return this.islands;
         return this.barrens;
     }
 }
