2f59845a68b38a931c1432f251bff87b8c0448c2
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-06-03 21:45:15 +0100

    Barrel book entry.

--- src/main/java/net/gourmand/core/datagen/book/guide/entries/homesteading/BarrelEntry.java
+++ src/main/java/net/gourmand/core/datagen/book/guide/entries/homesteading/BarrelEntry.java
@@ -5,12 +5,16 @@ import com.klikli_dev.modonomicon.api.datagen.EntryBackground;
 import com.klikli_dev.modonomicon.api.datagen.EntryProvider;
 import com.klikli_dev.modonomicon.api.datagen.book.BookIconModel;
 import com.klikli_dev.modonomicon.api.datagen.book.page.BookCraftingRecipePageModel;
+import com.klikli_dev.modonomicon.api.datagen.book.page.BookImagePageModel;
 import com.klikli_dev.modonomicon.api.datagen.book.page.BookSpotlightPageModel;
+import com.klikli_dev.modonomicon.api.datagen.book.page.BookTextPageModel;
 import com.mojang.datafixers.util.Pair;
 import net.dries007.tfc.TerraFirmaCraft;
 import net.dries007.tfc.common.TFCTags;
+import net.dries007.tfc.common.blocks.TFCBlocks;
+import net.dries007.tfc.common.blocks.wood.Wood;
 import net.dries007.tfc.common.items.TFCItems;
-import net.dries007.tfc.util.Metal;
+import net.gourmand.core.AncientGroundCore;
 import net.gourmand.core.modonomicon.datagen.BookBarrelFluidInstantPageModel;
 import net.gourmand.core.modonomicon.datagen.BookBarrelInstantPageModel;
 import net.gourmand.core.modonomicon.datagen.BookBarrelSealedPageModel;
@@ -35,11 +39,13 @@ public class BarrelEntry extends EntryProvider {
 
         this.pageTitle(entryName());
         this.pageText("""
-                The Barrel is a device that can hold both items and fluids.
-                The central slot is used to hold items.
-                Fluids are shown in the tank on the left side, and can be added to the barrel by placing a filled bucket or jug in the top left slot.
-                They can be removed by placing an empty fluid container in the same slot. Using Right Click on the block with a bucket also works.
+                The **Barrel** is a device that holds items and fluids. The barrel can be used for barrel recipes or to simply store fluid.
+                \\
+                \\
+                Barrels can be **Sealed**, this is toggled with the grey button, or by using **Shift + Right-Click** on the barrel with an empty hand.
                \s""");
+
+        //When nearly full of fluid, barrels in your inventory can cause **Overburdening** and if you hold two filled barrels at once causes **Pinning**.
 
         // page 2: crafting recipe.
         this.page("page2", () -> BookCraftingRecipePageModel.create()
@@ -52,50 +58,147 @@ public class BarrelEntry extends EntryProvider {
                The barrel is made from seven lumber. Requiring a saw to make the lumber.
                \s""");
 
-        // page 3: anvil recipe.
-        this.page("page3", () -> BookBarrelInstantPageModel.create()
+        // page 3: explain barrels.
+        this.page("page3", () -> BookTextPageModel.create()
+                .withTitle(this.context().pageTitle())
                 .withText(this.context().pageText())
+        );
+
+        this.pageTitle(entryName());
+        this.pageText("""
+                The central slot holds items. Which hold the inputs and outputs of recipes.
+                \\
+                \\
+                Fluids are shown in the tank on the left, and can be added by placing a container, such as a bucket in the top left slot.
+                Place an empty fluid container in the same slot to remove fluids. **Right-Clicking** on the block with a container also works.
+               \s""");
+
+        // page 4: interface.
+        this.page("page4", () -> BookImagePageModel.create()
+                .withText(this.context().pageText())
+                .withImages(ResourceLocation.fromNamespaceAndPath(AncientGroundCore.MODID, "textures/book/homesteading/barrel_interface.png"))
+        );
+
+        this.pageTitle(entryName());
+        this.pageText("""
+                The barrel interface.
+               \s""");
+
+        // page 5: how to do recipes.
+        this.page("page5", () -> BookTextPageModel.create()
+                .withText(this.context().pageText())
+                .withTitle(this.context.pageTitle())
+        );
+
+        this.pageTitle(entryName());
+        this.pageText("""
+        To do a Barrel recipe, fill the barrel with the correct ratio of fluid and items. Certain recipes require the barrel to be **Sealed** for a period of time, transforming once the timer runs out.
+        \\
+        \\
+        If the contents do not match the required ratio, the excess fluid or items will be lost.
+        """);
+
+        // page 6: how to do recipes.
+        this.page("page6", () -> BookTextPageModel.create()
+                .withText(this.context().pageText())
+                .withTitle(this.context.pageTitle())
+        );
+
+        this.pageTitle(entryName());
+        this.pageText("""
+        However if the recipe converts instantly, you will need enough items to fully convert the fluid.
+        \\
+        \\
+        If the the amount of items created by the recipe exceeds 64, the items will buffer, and appear in the slot when it is emptied.
+        """);
+
+        // page 7: instant barrel recipes.
+        this.page("page7", () -> BookBarrelInstantPageModel.create()
+                .withText(this.context().pageText())
+                .withTitle1("Limewater")
                 .withRecipeId1(ResourceLocation.fromNamespaceAndPath(TerraFirmaCraft.MOD_ID, "barrel/limewater"))
                 .withRecipeId2(ResourceLocation.fromNamespaceAndPath(TerraFirmaCraft.MOD_ID, "barrel/clay_ball"))
         );
 
-        this.pageTitle(entryName());
+        this.pageTitle("Examples");
         this.pageText("""
-                Testing Instant (item) barrel recipes.
-               \s""");
+        Example: of the instant barrel recipes that create **Limewater** and **Clay**.
+        """);
 
-        // page 4: welding recipe.
-        this.page("page4", () -> BookBarrelSealedPageModel.create()
+        // page 8: instant barrel recipes.
+        this.page("page8", () -> BookBarrelSealedPageModel.create()
                 .withText(this.context().pageText())
-                .withRecipeId1(ResourceLocation.fromNamespaceAndPath(TerraFirmaCraft.MOD_ID, "barrel/glue"))
-                .withRecipeId2(ResourceLocation.fromNamespaceAndPath(TerraFirmaCraft.MOD_ID, "barrel/jute_fiber"))
+                .withRecipeId1(ResourceLocation.fromNamespaceAndPath(TerraFirmaCraft.MOD_ID, "barrel/jute_fiber"))
+                .withRecipeId2(ResourceLocation.fromNamespaceAndPath(TerraFirmaCraft.MOD_ID, "barrel/glue"))
+        );
+
+        this.pageTitle("Examples");
+        this.pageText("""
+        Example: of the sealed barrel recipes that create **Jute Fiber** and **Glue**.
+        """);
+
+        // page 9: instant barrel recipes.
+        this.page("page9", () -> BookSpotlightPageModel.create()
+                .withText(this.context().pageText())
+                .withItem(TFCItems.WOODEN_BUCKET)
         );
 
         this.pageTitle(entryName());
         this.pageText("""
-                Testing Sealed barrel recipes.
-               \s""");
+        Some barrel recipes require the mixing of two fluids.
+        \\
+        \\
+        To do this, place the first fluid into the barrel and attempt to place the second fluid into the barrel.
+        """);
 
-        // page 5: welding recipe.
-        this.page("page5", () -> BookBarrelFluidInstantPageModel.create()
+        // page 10: instant barrel recipes.
+        this.page("page10", () -> BookBarrelFluidInstantPageModel.create()
                 .withText(this.context().pageText())
                 .withRecipeId1(ResourceLocation.fromNamespaceAndPath(TerraFirmaCraft.MOD_ID, "barrel/brine"))
+                .withTitle1(this.context().pageTitle())
+        );
+
+        this.pageTitle("Brine");
+        this.pageText("""
+        Example: of the fluid instant barrel recipes that create **Brine**.
+        """);
+
+        // page 11: barrel tips.
+        this.page("page11", () -> BookTextPageModel.create()
+                .withText(this.context().pageText())
+        );
+
+        this.pageTitle("Barrel Tips");
+        this.pageText("""
+        Some tips:   \s
+        - Empty Barrels can be filled by clicking on a fluid source block.
+        - During rain, open barrels will slowly fill up with water.
+        - Icicles melting above barrels adds water to them.
+        - Sealing a barrel will eject items that are not in the center slot.
+        - Barrels can be placed on their side by placing them against the side of a block.
+        - Side ways barrels can be stacked using **Barrel Racks**.
+        """);
+
+        // page 12: sideways barrels.
+        this.page("page12", () -> BookImagePageModel.create()
+                .withText(this.context().pageText())
+                .withImages(ResourceLocation.fromNamespaceAndPath(AncientGroundCore.MODID, "textures/book/homesteading/barrel_drip.png"))
         );
 
         this.pageTitle(entryName());
         this.pageText("""
-                Testing Instant (fluid) barrel recipes
-               \s""");
+        Barrels on their side which are **Open**, can drip their contents into fluid containers.
+        """);
     }
 
     @Override
     protected String entryName() {
-        return "Armour";
+        return "Barrels";
     }
 
     @Override
     protected String entryDescription() {
-        return "About Armour.";
+        return "About Barrels.";
     }
 
     @Override
@@ -105,11 +208,11 @@ public class BarrelEntry extends EntryProvider {
 
     @Override
     protected BookIconModel entryIcon() {
-        return BookIconModel.create(TFCItems.METAL_ITEMS.get(Metal.COPPER).get(Metal.ItemType.CHESTPLATE));
+        return BookIconModel.create(TFCBlocks.WOODS.get(Wood.OAK).get(Wood.BlockType.BARREL));
     }
 
     @Override
     protected String entryId() {
-        return "armour";
+        return "barrel";
     }
 }
