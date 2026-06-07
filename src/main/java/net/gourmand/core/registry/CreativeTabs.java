9c207d398aed8147b93f5558fee1defa9adba18e
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-06-07 13:25:31 +0100

    Add bundle to creative menu (and therefore recipe viewers)

--- src/main/java/net/gourmand/core/registry/CreativeTabs.java
+++ src/main/java/net/gourmand/core/registry/CreativeTabs.java
@@ -14,6 +14,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -292,7 +293,7 @@ public class CreativeTabs {
 
     private static void fillMisc(CreativeModeTab.ItemDisplayParameters parameters, CreativeModeTab.Output out)
     {
-
+        out.accept(Items.BUNDLE);
         out.accept(CoreBlocks.SLUDGE.get());
         out.accept(CoreBlocks.PRISMATIC_ICE.get());
         out.accept(CoreBlocks.LEAD_BULB_BLOCK.get());
