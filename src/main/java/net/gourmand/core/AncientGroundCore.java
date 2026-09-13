9264c450d9da8cc0c574e9b6d8bb486cc2799745
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-09-13 12:15:46 +0100

    comment unneeded code.

--- src/main/java/net/gourmand/core/AncientGroundCore.java
+++ src/main/java/net/gourmand/core/AncientGroundCore.java
@@ -63,7 +63,8 @@ public class AncientGroundCore
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
-        ServerEventHandler.init(NeoForge.EVENT_BUS, modContainer);
+        // implement when needed
+        //ServerEventHandler.init(NeoForge.EVENT_BUS, modContainer);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         //modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
